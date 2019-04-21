50e46b8befbd62966e0fca1daa7dd2c37bbdbdcc
elior kertsman <eliorke@ac.sce.ac.il>
2019-04-21 16:01:01 +0300

    code smell

--- src/you_get/extractors/bigthink.py
+++ src/you_get/extractors/bigthink.py
@@ -2,6 +2,7 @@
 
 from ..common import *
 from ..extractor import VideoExtractor
+from ..extractor import*
 
 import json
 
@@ -65,12 +66,6 @@ class Bigthink(VideoExtractor):
         for i in link_list:
             self.stream_types.append({'id': str(i[0])})
             self.streams[i[0]] = {'url': i[1]}
-
-    def extract(self, **kwargs):
-        for i in self.streams:
-            s = self.streams[i]
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
 
 site = Bigthink()
 download = site.download_by_url
